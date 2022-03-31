a525479352e2233b870f1e748d35a22c846591d7
BillChee123 <colo316@hotmail.com>
2022-03-31 23:58:35 +0800

    Add experiences section and improve styling

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 
 const aboutMeAnchor = document.getElementById('about-me-anchor');
+const experiencesAnchor = document.getElementById('experiences-anchor');
 const skillsAnchor = document.getElementById('skills-anchor');
 const projectsAnchor = document.getElementById('projects-anchor');
 const contactsAnchor = document.getElementById('contacts-anchor');
 
 const aboutNavSelector = document.getElementById('about-nav-item');
+const experiencesNavSelector = document.getElementById('experiences-nav-item');
 const skillsNavSelector = document.getElementById('skills-nav-item');
 const projectsNavSelector = document.getElementById('projects-nav-item');
 const contactNavSelector = document.getElementById('contact-nav-item');
@@ -17,6 +19,10 @@ aboutMeButton.addEventListener('click', arg => {
 
 aboutNavSelector.addEventListener('click', arg => {
     aboutMeAnchor.scrollIntoView({ behavior: "smooth" });
+});
+
+experiencesNavSelector.addEventListener('click', arg => {
+    experiencesAnchor.scrollIntoView({ behavior: "smooth" });
 });
 
 skillsNavSelector.addEventListener('click', arg => {
